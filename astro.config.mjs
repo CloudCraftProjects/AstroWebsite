7ded8737981fd7ad5d5a3ba5ac3c6c76ad5bcf1a
booky10 <boooky10@gmail.com>
2024-05-19 21:02:12 +0200

    Change site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ccbetasite.pages.dev",
+  site: "https://cloudcraftmc.de",
   output: "static",
   compressHTML: false,
   integrations: [
