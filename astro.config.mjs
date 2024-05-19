5898a45007b6bdb62d9a540bedc16407647fe205
booky10 <boooky10@gmail.com>
2024-05-19 03:35:25 +0200

    Add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ccbetasite.pages.dev",
   output: "static",
   compressHTML: false,
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes("404"),
+    }),
+  ],
 });
