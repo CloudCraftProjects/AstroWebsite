e772205f26eee9e44876ea46c863452fe7fc2dea
booky <booky@booky.dev>
2026-09-06 04:16:20 +0200

    Update dependencies, fix astro check errors
    Also rewrites player counter component to preact

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
-import {defineCollection, z} from "astro:content";
-import {file, glob} from "astro/loaders";
+import {defineCollection} from "astro:content";
+import {glob} from "astro/loaders";
+import {z} from "zod";
 
 const categories = defineCollection({
     loader: glob({pattern: "*/meta.yml", base: "./src/projects"}),
